3ec3b669087300443f8a8c8e4a3c589e43256ca4
Carlos Celis <celisflenbers@gmail.com>
2016-03-24 00:15:12 -0430

    Abstrayendo las funciones
    Mejorando busqueda en simple.html
    Agregando búsqueda adicional a pypi

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Funciones para el Telegram Bot de PyVe
 
 Más info sobre este Telegram Bot https://github.com/pyve/PyVenezuelaBot
+
+```bash
+$ git clone git@github.com:celisflen-bers/pyvebot_functions.git
+$ cd pyvebot_functions
+$ ./exec.py
+```
+
--- functions.py
+++ functions.py
@@ -6,31 +6,64 @@
 # Repo: https://github.com/celisflen-bers/pyvebot_functions
 
 import xmlrpclib
+import re
+from lxml.etree import fromstring
+from lxml.cssselect import CSSSelector
 
-pypi_simple   = "https://pypi.python.org/simple/index.html"
-pypi_base_url = "https://pypi.python.org/pypi/"
+MAX_PACKAGE_RETURN = 50
+
+pypi_base_url   = "https://pypi.python.org/"
+pypi_index = pypi_base_url + "pypi/"
+
+pypi_simple_url = pypi_base_url + "simple/index.html"
+# wget https://pypi.python.org/simple/index.html -O simple.html
+pypi_simple_local = "simple.html"
+
+with open(pypi_simple_local) as f:
+  h = fromstring(f.read())
+  sel = CSSSelector("a")
+  packages = {}
+  for e in sel(h):
+    packages[e.text] = e.get("href")
 
 
+# XMLRPC connection
 def srv_conn():
-  #https://wiki.python.org/moin/PyPIXmlRpc
-  srv_pypi = xmlrpclib.ServerProxy(pypi_base_url)
+  # https://wiki.python.org/moin/PyPIXmlRpc
+  srv_pypi = xmlrpclib.ServerProxy(pypi_index)
   return srv_pypi
 
 
-def package_located(pkg):
-
-  pkg_info = srv_conn()
-  rel = pkg_info.package_releases(pkg)
-  lastest_rel = rel and rel[0] or None
-
-  pkg_data = pkg_info.release_data(pkg, lastest_rel)
-  return pkg_data
-
-
-def simplesearch(pkg):
-  print pkg
+# Multi match
+def simple_search(pkg):
+  # https://wiki.python.org/moin/PyPISimple
+  results = {}
+  regexp = re.compile(pkg, re.IGNORECASE)
+  for x in packages:
+    if regexp.search(x) is not None:
+      results[x] = packages[x]
+  return results
 
 
+# Función pypi en el bot
+def pkg_info(pkg):
+  # https://wiki.python.org/moin/PyPIXmlRpc
+  rel_len = 0
+  res = simple_search(pkg)
+  res_len = len(res)
+  if res_len > 0:
+    pkg_info = srv_conn()
+    rel = pkg_info.package_releases(pkg)
+    rel_len = len(rel)
+    pkg_data = None
+    if rel_len > 0:
+      lastest_rel = rel and rel[0] or None
+      pkg_data = pkg_info.release_data(pkg, lastest_rel)
+    return pkg_data
+
+
+# Función pysearch en el bot
 def xmlrpcsearch(pkg, spec='name'):
+  # https://wiki.python.org/moin/PyPIXmlRpc
   pkg_info = srv_conn()
-  return pkg_info.search({spec:pkg}, {'_pypi_ordering': True})
+  return pkg_info.search({'name':pkg}, {'_pypi_ordering': True})
